6bc7ab8d84831d10fca16124a7f14de6a2bac7c0
Андрей Новиков <andreinovikov@Andrejs-MBP.hsd1.ca.comcast.net>
2020-04-09 12:04:12 -0700

    add appBar

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:ready_berry/widgets/berry_selector.dart';
 import 'package:ready_berry/widgets/category_selector.dart';
 
@@ -7,9 +8,79 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        elevation: 0.0,
+      appBar: new AppBar(
+        iconTheme: new IconThemeData(color: Color(0xFFE9BDDA)),
         backgroundColor: Color(0xFFFDFCF8),
+        elevation: 0.0,
+      ),
+      drawer: new Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            DrawerHeader(
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment.bottomCenter,
+                  end: Alignment.topCenter,
+                  colors: [
+                    Color(0xFFD79EC2),
+                    Color(0xFFFDF0DF),
+                  ],
+                ),
+              ),
+              child: SvgPicture.asset('assets/img/logo-small.svg',
+                  fit: BoxFit.scaleDown),
+              margin: null,
+            ),
+            Container(
+              height: 50.0,
+              margin: EdgeInsets.only(bottom: 20.0),
+              color: Color(0xFFE9BDDA),
+            ),
+            ListTile(
+              leading: SvgPicture.asset('assets/img/info.svg'),
+              title: Text(
+                'About',
+                style: TextStyle(
+                  fontSize: 20,
+                  color: Color(0xFF4F4F4F),
+                ),
+              ),
+              onTap: () {
+                // Update the state of the app.
+                // ...
+              },
+            ),
+            ListTile(
+              leading: SvgPicture.asset('assets/img/marker.svg'),
+              title: Text(
+                'Change the location',
+                style: TextStyle(
+                  fontSize: 20,
+                  color: Color(0xFF4F4F4F),
+                ),
+              ),
+              onTap: () {
+                // Update the state of the app.
+                // ...
+              },
+            ),
+            ListTile(
+              leading: SvgPicture.asset('assets/img/coffee.svg'),
+              title: Text(
+                'Buy me a coffee :)',
+                style: TextStyle(
+                  fontSize: 20,
+                  color: Color(0xFF4F4F4F),
+                ),
+              ),
+              onTap: () {
+                // Update the state of the app.
+                // ...
+              },
+            ),
+          ],
+        ),
       ),
       body: Container(
           width: MediaQuery.of(context).size.width,
